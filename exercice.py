7b6ec7c546a20fb4df4420ef546ebe0ddda9c617
Remi4001 <remilabelle@outlook.com>
2022-09-21 09:41:56 -0400

    Compléter join_integers()

--- exercice.py
+++ exercice.py
@@ -8,24 +8,28 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [max(liste) for liste in numbers]
+    return [max(liste) for liste in numbers]
+
 
 def join_integers(numbers):
-	return 0
+    return int("".join([str(number) for number in numbers]))
+
 
 def generate_prime_numbers(limit):
-	return [0]
+    return [0]
+
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+    return [""]
+
 
 if __name__ == "__main__":
-	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
-	print("")
-	print(join_integers([111, 222, 333]))
-	print(join_integers([69, 420]))
-	print("")
-	print(generate_prime_numbers(17))
-	print("")
-	print(combine_strings_and_numbers(["A", "B"], 2, None))
-	print(combine_strings_and_numbers(["A", "B"], 5, 2))
+    print(get_maximums([[1, 2, 3], [6, 5, 4], [10, 11, 12], [8, 9, 7]]))
+    print("")
+    print(join_integers([111, 222, 333]))
+    print(join_integers([69, 420]))
+    print("")
+    print(generate_prime_numbers(17))
+    print("")
+    print(combine_strings_and_numbers(["A", "B"], 2, None))
+    print(combine_strings_and_numbers(["A", "B"], 5, 2))
